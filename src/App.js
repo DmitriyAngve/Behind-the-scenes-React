f3c57240fbdaed110739088d73e0f70e7996d9f9
Dmitriy Angve <kirirch129@gmail.com>
2022-12-12 14:00:55 +0300

    #

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App running!"); // se this whenever we clicked a button and yet
+
+  // This function triggee then the button is clicked
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
 
 export default App;
+
+//{showParagraph && <p>This is new!</p>} - element conditionally. Initially not shown - useState!
